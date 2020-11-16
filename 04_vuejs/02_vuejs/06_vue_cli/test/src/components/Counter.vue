7dbbc5a70d4bf6bbec6fcef703ab3c5dc3b3be8f
Óliver Hierro <ohierro@gmail.com>
2020-11-16 13:18:58 +0100

    Added 04 vuejs exercises

---
+++ 04_vuejs/02_vuejs/06_vue_cli/test/src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Esto es un componente
+    <span>{{counter}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  created() {
+    setInterval(() => this.counter++, 1000)
+  }
+}
+</script>
+
+<style scoped>
+span {
+  color: cadetblue;
+}
+</style>
