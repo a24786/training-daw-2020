77f511dc173fa05f3dfa737e3e399b7e1807199e
Óliver Hierro <ohierro@hiberus.com>
2021-01-08 11:05:39 +0100

    dd

---
+++ 04_vuejs/02_vuejs/06_vue_cli/test2/src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <div class="d-flex flex-row justify-content-center mb-4">
+      <div class="w-75 d-flex flex-row justify-content-center">
+        <b-form-input
+            v-model="text"
+            placeholder="Enter your name"
+            class="col-9"
+          ></b-form-input>
+        <b-button
+          variant="danger"
+          class="col-3"
+          >Peligro</b-button>
+      </div>
+    </div>
+
+    <div class="d-flex flex-row justify-content-center">
+      <b-list-group class="w-75">
+        <b-list-group-item>Cras justo odio</b-list-group-item>
+        <b-list-group-item>Dapibus ac facilisis in</b-list-group-item>
+        <b-list-group-item>Morbi leo risus</b-list-group-item>
+        <b-list-group-item>Porta ac consectetur ac</b-list-group-item>
+        <b-list-group-item>Vestibulum at eros</b-list-group-item>
+      </b-list-group>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
