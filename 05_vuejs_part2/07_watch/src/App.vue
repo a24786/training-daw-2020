93d4307f3a1680e3b8d0f0dbf47521e252594fd7
Óliver Hierro <ohierro@hiberus.com>
2021-01-15 08:24:20 +0100

    Added example 07

---
+++ 05_vuejs_part2/07_watch/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Question></Question>
+  </div>
+</template>
+
+<script>
+import Question from './components/Question'
+
+export default {
+  name: 'App',
+  components: {
+    Question
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
