6daf653905df3c3b7ff5ac998a7aa3ef481f6964
Óliver Hierro <ohierro@hiberus.com>
2021-01-15 08:15:37 +0100

    Examples 05 and 06

---
+++ 05_vuejs_part2/05_quotes/src/components/Quote.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+        <div>{{quote}}</div>
+        <div>{{author}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'QuoteWidget',
+    data() {
+        return {
+            quote: '',
+            author: ''
+        }
+    },
+    created() {
+        // fetch('https://quotes.rest/qod?language=en')
+        fetch('https://favqs.com/api/qotd')
+            .then(response => response.json())
+            .then(response => {
+                // this.quote = response.contents.quotes[0].quote
+                this.quote = response.quote.body
+                this.author = response.quote.author
+            })
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 05_vuejs_part2/06_stocks/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      quotes: [
+        { id: '3M', name: '3M', value: 166.41, change: 0.13, percentChange: 0.08	},
+        { id: 'APPL', name: 'Apple', value: 128.91, change: -1.98, percentChange: -1.51	},
+        { id: 'AXP', name: 'American Express', value: 123.78, change: 1.38, percentChange: 1.13	},
+        { id: 'BA', name: 'Boing Co', value: 208.21, change: 2.7, percentChange: 1.3	},
+        { id: 'CSCO', name: 'Cisco Systems Inc', value: 44.85, change: -0.21, percentChange: -0.46	}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
