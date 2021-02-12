2bb4ed18cec7394247905c7fd8588aea61eb28d7
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2021-02-12 16:19:06 +0100

    Added new example

---
+++ 05_vuejs_part2/10_my_first_router/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div>This is home</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
