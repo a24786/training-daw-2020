718c15cb58446a44ac8536ad37f1dcd73f412b03
Óliver Hierro <ohierro@hiberus.com>
2021-02-12 10:41:56 +0100

    Added dynamic routing example

---
+++ 05_vuejs_part2/11_dynamic_routing/sample-routing/src/views/Info.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <div>Editing {{number}}</div>
+      <div>Route param {{$route.params.id}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+        // mounted() {
+        //     this.fetchData()
+        // },
+    data() {
+        return {
+            number: 0
+        }
+    },
+    // beforeRouteUpdate(to, from, next) {
+    //     this.number = to.params.id
+    //     next()
+    // },
+    watch: {
+        $route: "fetchData"
+    },
+    methods: {
+        fetchData() {
+            console.log(`edit ${this.$route.params.id}`)
+            this.number = this.$route.params.id
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
